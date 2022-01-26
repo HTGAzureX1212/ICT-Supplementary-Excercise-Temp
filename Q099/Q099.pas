2d7b531192d87ca49986976378a645beb208683a
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-01-26 09:11:14 +0800

    add Q099 solution

---
+++ Q099/Q099.pas
@@ -0,0 +1,31 @@
+program Q099;
+var
+    randomInteger: integer;
+    input: integer;
+    guesses: integer;
+begin
+    randomize;
+    randomInteger := random(100) + 1;
+    
+    write('Enter your guess: ');
+    readln(input);
+    
+    repeat begin
+        if input > randomInteger then
+        begin
+            write('The secret number is smaller than your guess. Try again: ');
+            readln(input);
+        end
+        else if input < randomInteger then
+        begin
+            write('The secret number is larger than your guess. Try again: ');
+            readln(input);
+        end;
+        
+        guesses := guesses + 1;
+    end
+    until input = randomInteger;
+    
+    writeln('Exactly! Well done.');
+    writeln('Number of guesses = ', guesses);
+end.
